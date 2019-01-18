05c20b81df0cc76cdb45acbeb71e57c70d208d02
Devin Sweeting <devinsweeting@gmail.com>
2019-01-18 11:40:53 -0800

    Add callDoctor function

--- src/api.js
+++ src/api.js
@@ -6,7 +6,7 @@ export class Api {
   callSymptons() {
     return new Promise(function(resolve, reject) {
       let request = new XMLHttpRequest();
-      let url = `https://api.betterdoctor.com/2016-03-01/conditions?user_key=272bed7e3d95b8d1a826150cd39add00`;
+      let url = `https://api.betterdoctor.com/2016-03-01/conditions?user_key=${process.env.USER_KEY}`;
       request.onload = function() {
         if (this.status === 200) {
           resolve(request.response);
@@ -22,7 +22,7 @@ export class Api {
   callDoctor(symptons, address, distance, sortby) {
     return new Promise(function(resolve, reject) {
       let request = new XMLHttpRequest();
-      let url = `https://api.betterdoctor.com/2016-03-01/conditions?user_key=272bed7e3d95b8d1a826150cd39add00`;
+      let url = `https://api.betterdoctor.com/2016-03-01/doctors?user_key=${process.env.USER_KEY}&location=${address},${distance}&query=${symptons}&sort=${sortby}`;
       request.onload = function() {
         if (this.status === 200) {
           resolve(request.response);
--- src/main.js
+++ src/main.js
@@ -6,14 +6,14 @@ import 'bootstrap/dist/css/bootstrap.min.css';
 
 $(document).ready(function() {
   // Calls list of symptons to be displayed in DOM after loading
-  // let symptons = new Api();
-  // let symptonsPromise = symptons.callSymptons();
-  // symptonsPromise.then(function(response) {
-  //   let selections = JSON.parse(response);
-  //   selections.data.forEach(function(sympton) {
-  //     $("#symptons").append("<option value=" + sympton.uid + ">" + sympton.name + "</option>");
-  //   });
-  // });
+  let symptons = new Api();
+  let symptonsPromise = symptons.callSymptons();
+  symptonsPromise.then(function(response) {
+    let selections = JSON.parse(response);
+    selections.data.forEach(function(sympton) {
+      $("#symptons").append("<option value=" + sympton.uid + ">" + sympton.name + "</option>");
+    });
+  });
 
   // Where user selects search parameters to find local doctor
   $('#doctor-search').submit(function(event) {
@@ -22,8 +22,9 @@ $(document).ready(function() {
     const userAddress = $("#address").val();
     const userDistance =$("#distance").val();
     const sortBy = $("#sort-by").val();
-    const doctorSearch = new Api();
-    l
-    debugger;
+    // const doctorSearch = new Api();
+    // const doctorsPromise = doctorSearch.callDoctor(userSymptons, userAddress, userDistance, sortBy);
+    //   let doctors = JSON.parse(response);
+    // debugger;
     });
   });
